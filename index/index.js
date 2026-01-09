f062fbe253eefe339a7657bbcc945d80969ad075
luishhope <lugui13579@gmail.com>
2026-01-09 00:22:07 -0300

    Melhorias no header, adição de barra de pesquisa, adição de catalogo, adição de sessoes e cards dos produtos, adição do carrinho. (tudo so visual ainda nao e funcional).

--- index/index.js
+++ index/index.js
@@ -3,3 +3,10 @@ const shirts = document.querySelectorAll('.hero-images img');
 shirts.forEach((shirt, index) => {
   shirt.style.animation = `float 4s ease-in-out ${index * 0.3}s infinite`;
 });
+
+document.querySelectorAll('.expand-arrow').forEach(button => {
+  button.addEventListener('click', () => {
+    const section = button.closest('.store-section');
+    section.classList.toggle('expanded');
+  });
+});
